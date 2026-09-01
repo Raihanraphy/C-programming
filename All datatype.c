ddd1ea0844b9558751e4cd286434aa840e586377
Raihan Rahman <raihanraphy@gmail.com>
2026-09-01 22:57:18 +0600

    Create All datatype.c

---
+++ All datatype.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a=2;
+    float b=2.5f;
+    double c=3.45664;
+    char d='A';
+
+    printf("Interger : %d\n", a);
+    printf("Float : %2f\n", b);
+    printf("Double : %lf\n", c);
+    printf("Character : %c\n", d);
+    return 0;
+
+}
