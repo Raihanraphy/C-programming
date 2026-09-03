2d07b14bfc574829e5b821963f71ce5b6ea70334
Raihan Rahman <raihanraphy@gmail.com>
2026-09-03 20:33:38 +0600

    Create Fibonacci Recursion.c

---
+++ Fibonacci Recursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int fibonacci(int i){
+    if (i==0){
+        return 0;
+    }
+    else if ( i==1){
+        return 1;
+    }
+    else{
+        return fibonacci(i-1) + fibonacci (i-2);
+    }
+}
+int main() {
+    int n, i;
+
+    printf("Enter a number: \n");
+    scanf("%d", &n);
+
+    printf("The fibonacci series : ");
+    for (i=0; i<n; i++)
+    {
+        printf("%d",fibonacci(i));
+    }
+
+    return 0;
+}
